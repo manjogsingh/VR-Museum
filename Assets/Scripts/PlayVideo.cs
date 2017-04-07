0d1607e494cccb973a7cb1034c4c94ff08bb6579
manjogsingh <manjogsingh@gmail.com>
2017-04-07 07:22:28 +0530

    edit : model and added video

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.mainTexture=movie;
+		//MovieTexture movie=(MovieTexture)gameObject.GetComponent<Renderer>().material.mainTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(movie.isPlaying);
+	}
+	public void Play()
+	{
+		Debug.Log("lol");
+		Debug.Log(movie.isPlaying);
+		movie.Play();
+	}
+}
