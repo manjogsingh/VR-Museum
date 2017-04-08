38accfbc640ee6e4a3164da6df294036fe65ad26
manjogsingh <manjogsingh@gmail.com>
2017-04-09 00:16:34 +0530

    Models videos added

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+
+    public Texture play;
+    public Texture pause;
+    Transform videoButton;
+    VideoPlayer vPlayer;
+    VideoPlayer[] videos;
+    void Start()
+    {
+        vPlayer = gameObject.GetComponent<VideoPlayer>();
+        videoButton = transform.FindChild("Button");
+        videos = GameObject.FindObjectsOfType<VideoPlayer>();
+    }
+
+    public void PlayVideo()
+    {
+        if (!vPlayer.isPlaying)
+        {
+            StopAll();
+            videoButton.gameObject.SetActive(false);
+            vPlayer.Play();
+        }
+        else
+        {
+            videoButton.GetComponent<Renderer>().material.mainTexture = play;
+            videoButton.gameObject.SetActive(true);
+            vPlayer.Pause();
+        }
+    }
+    void StopAll()
+    {
+        for (int i = 0; i < videos.Length; i++)
+        {
+            videos[i].Pause();
+            videos[i].transform.FindChild("Button").gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Waypoint : MonoBehaviour
+{
+
+    public Transform startPosition;
+    public Transform endPosition;
+    public float speed = 0.05f;
+    bool startLerp;
+    GameObject currentObject;
+    void Start()
+    {
+        startLerp = false;
+        //startPosition=transform;
+        StartCoroutine(waitForStart());
+    }
+    public void Move(Transform current)
+    {
+        endPosition = current;
+        startLerp = !startLerp;
+    }
+    IEnumerator waitForStart()
+    {
+        while (!startLerp)
+        {
+            yield return new WaitForSeconds(1.0f);
+        }
+        StartCoroutine(MoveTo());
+    }
+    IEnumerator MoveTo()
+    {
+        startLerp = false;
+        float timeToStart = Time.time;
+        while (!startLerp)
+        {
+            float temp = (Time.time - timeToStart) * speed;
+            startPosition.position = Vector3.Lerp(startPosition.position, endPosition.position, temp);
+            //endPosition.localScale = Vector3.Lerp(endPosition.localScale, new Vector3(0, 0, 0), temp);
+            yield return null;
+        }
+        StartCoroutine(waitForStart());
+    }
+}
